74a18cea3fa9d2f25a6f4e9c0c4eadbe3a57f130
Natalie Ramirez <61169728+natalieramirez22@users.noreply.github.com>
2020-04-30 19:59:49 -0400

    StartUp.java

--- StartUp.java
+++ StartUp.java
@@ -5,8 +5,14 @@ public class StartUp
   public static void main (String[] args)
   {
     Scanner kb = new Scanner(System.in);
+    
+    System.out.println("Enter a word or phrase: ");
     String str = kb.nextLine();
+    System.out.println("Enter a positive number: ");
     int num = kb.nextInt();
+    
+    inputString();
+    inputInt();
   }
   
   private static String inputString()
