4e56dd8c954bfda355c23afc7561669b9dca2f25
Natalie Ramirez <61169728+natalieramirez22@users.noreply.github.com>
2020-04-30 19:57:46 -0400

    StartUp.java
    class that works through methods, main method, scanner, and calling methods in main

---
+++ StartUp.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class StartUp 
+{ 
+  public static void main (String[] args)
+  {
+    Scanner kb = new Scanner(System.in);
+    String str = kb.nextLine();
+    int num = kb.nextInt();
+  }
+  
+  private static String inputString()
+  {
+    return str;
+  }
+  
+  private static int inputInt()
+  {
+    return num;
+  }
+}
